096f96c08543b90a98a64b01091b9ecd67a9d684
Carlos Daga <67453570+dicarlos-maker@users.noreply.github.com>
2024-07-14 20:40:32 +0200

    repo and files created

---
+++ javaScriptFundamentals/quiz.js
@@ -0,0 +1,22 @@
+function checkAnswer() {
+   
+    var correctAnswer = "4";
+
+    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+ 
+    var feedback = document.getElementById('feedback');
+
+    if (userAnswer) {
+     
+        if (userAnswer.value === correctAnswer) {
+            feedback.textContent = "Correct! Well done.";
+            feedback.style.color = "green";
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+            feedback.style.color = "red";
+        }
+    }
+}
+
+document.getElementById('submit-answer').addEventListener('click', checkAnswer);
